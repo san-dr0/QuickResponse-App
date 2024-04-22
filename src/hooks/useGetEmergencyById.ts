ed6e1e355e30f07f67d940e40b01de2d5b97d871
chappy <lumangyao112@gmail.com>
2024-04-22 23:37:34 +0800

    [fix][chappy] feature view emergency

---
+++ src/hooks/useGetEmergencyById.ts
@@ -0,0 +1,38 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {getEmergencyById} from '../service/emergency/Emergency.service';
+import {EmergencyDto} from '../dto/Emergency.dto';
+
+type Props = {
+  emergencyId: string;
+};
+
+export default function useGetEmergencyById(props: Props) {
+  const {emergencyId} = props;
+  const [data, setData] = useState<EmergencyDto | null>(null);
+  const [isRefresh, setIsRefresh] = useState<boolean>(false);
+
+  const sendRequest = useCallback(async () => {
+    try {
+      const resposne = await getEmergencyById(emergencyId);
+
+      console.log('geggg', resposne.data());
+      setData(resposne.data() as EmergencyDto);
+    } catch (error) {
+      console.log('err', error);
+    }
+  }, [emergencyId]);
+
+  useEffect(() => {
+    sendRequest();
+  }, []);
+
+  useEffect(() => {
+    sendRequest();
+  }, [isRefresh]);
+
+  return {
+    setIsRefresh,
+    isRefresh,
+    data,
+  };
+}
--- src/hooks/useOnReceiveFCM.ts
+++ src/hooks/useOnReceiveFCM.ts
@@ -1,18 +1,18 @@
 import messaging from '@react-native-firebase/messaging';
-import { EmergencyDto } from '../dto/Emergency.dto';
-import { UserType } from '../enums/User.enum';
-import { useAccountContext } from '../providers/AccountProvider';
-import { useAlertContext } from '../providers/AlertProvider';
+import {EmergencyDto} from '../dto/Emergency.dto';
+import {UserType} from '../enums/User.enum';
+import {useAccountContext} from '../providers/AccountProvider';
+import {useAlertContext} from '../providers/AlertProvider';
 
 export function useOnReceiveFirebaseCloudMessaging() {
-  const { setAlertRecords } = useAlertContext();
-  const { activeUserInformation } = useAccountContext();
+  const {setAlertRecords} = useAlertContext();
+  const {activeUserInformation} = useAccountContext();
 
   const onReceive = () => {
     const message = messaging().onMessage(async (remoteMessage: any) => {
       const parseMessage = remoteMessage;
 
-      console.log('wew', JSON.parse(parseMessage.data?.coordinate))
+      console.log('wew', JSON.parse(parseMessage.data?.coordinate));
 
       const payload: EmergencyDto = {
         type: parseMessage?.data?.type,
@@ -20,9 +20,9 @@ export function useOnReceiveFirebaseCloudMessaging() {
         responder: [],
         emergencyStatus: parseMessage?.data?.emergencyStatus,
         coordinate: JSON.parse(parseMessage?.data?.coordinate),
-        date: parseMessage?.data?.date
-      }
-      const emergencyId = parseMessage?.data?.emergencyId
+        date: parseMessage?.data?.date,
+      };
+      const emergencyId = parseMessage?.data?.emergencyId;
       const title = JSON.parse(remoteMessage?.data?.notification)?.title;
       const body = JSON.parse(remoteMessage?.data?.notification)?.body;
       //const emergency = JSON.parse(remoteMessage?.data);
@@ -33,9 +33,14 @@ export function useOnReceiveFirebaseCloudMessaging() {
       if (activeUserInformation?.account?.userType === UserType.RESPONDER) {
         console.log('IM IN responder');
 
-        setAlertRecords({ title, body, isActive: true, emergencyID: emergencyId, emergency: payload });
-      };
-
+        setAlertRecords({
+          title,
+          body,
+          isActive: true,
+          emergencyID: emergencyId,
+          emergency: payload,
+        });
+      }
     });
 
     return message;
@@ -44,17 +49,37 @@ export function useOnReceiveFirebaseCloudMessaging() {
   const onReceiveBackgroundMessage = () => {
     const backgroundMessage = messaging().setBackgroundMessageHandler(
       async (remoteMessage: any) => {
+        const parseMessage = remoteMessage;
+
+        console.log('wew', JSON.parse(parseMessage.data?.coordinate));
+
+        const payload: EmergencyDto = {
+          type: parseMessage?.data?.type,
+          sender: JSON.parse(parseMessage?.data?.sender),
+          responder: [],
+          emergencyStatus: parseMessage?.data?.emergencyStatus,
+          coordinate: JSON.parse(parseMessage?.data?.coordinate),
+          date: parseMessage?.data?.date,
+        };
+        //const emergency =
         const emergencyId = remoteMessage?.data?.emergencyId;
         const title = remoteMessage?.data?.notification?.title;
         const body = remoteMessage?.data?.notification?.body;
         console.log('REMOTE >> backgroundMessage');
         console.log(remoteMessage);
-        setAlertRecords({ title, body, isActive: true, emergencyID: emergencyId });
+
+        setAlertRecords({
+          title,
+          body,
+          isActive: true,
+          emergencyID: emergencyId,
+          emergency: payload,
+        });
       },
     );
 
     return backgroundMessage;
   };
 
-  return { onReceive, onReceiveBackgroundMessage };
+  return {onReceive, onReceiveBackgroundMessage};
 }
